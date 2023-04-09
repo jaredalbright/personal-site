af3247531c25d8963373d59944698b024c7f1e6b
jaredalbright <jaredalbright@vt.edu>
2023-04-09 19:00:31 -0400

    began menu

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import '../index.css'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="dp-menu">
+            <h1>Jared Albright</h1>
+            
+                <h2 className="dp-option">Developer</h2>
+                <h2 className="dp-option">Photography</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Header
